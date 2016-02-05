cf0b0505cbef78d4e273712f1bee9cbeab828328
mishgan95 <misha-varaksin10@mail.ru>
2016-02-06 00:25:23 +0700

    исправил

--- XRect/XRect/MainWindow.xaml.cs
+++ XRect/XRect/MainWindow.xaml.cs
@@ -175,19 +175,7 @@ namespace XRect
 
         private void btnSave_Click_1(object sender, RoutedEventArgs e)
         {
-            RenderTargetBitmap rtb = new RenderTargetBitmap((int)cnvsMain.RenderSize.Width,
-    (int)cnvsMain.RenderSize.Height, 96d, 96d, System.Windows.Media.PixelFormats.Default);
-            rtb.Render(cnvsMain);
-
-            var crop = new CroppedBitmap(rtb, new Int32Rect(0, 0, 450, 450));
-
-            BitmapEncoder pngEncoder = new PngBitmapEncoder();
-            pngEncoder.Frames.Add(BitmapFrame.Create(crop));
-
-            using (var fs = System.IO.File.OpenWrite("logo.png")) 
-            {
-                pngEncoder.Save(fs);
-            }
+            SaveLoadImg.SaveImg(cnvsMain);
         }
 
         private void btnLoad_Click_1(object sender, RoutedEventArgs e)
@@ -195,64 +183,25 @@ namespace XRect
             cnvsMain.Children.Clear();
 
             loadingImg = new System.Drawing.Bitmap("logo.png");
-            LIR = new List<ImgRec>(); 
             LRB = new List<RectBox>();
-
-            LIL = new List<ImgLine>();
-
-            drawRect(0);
-            drawRect(1); 
-
-            drawLine(0);
-            drawLine(1);
-        }
-
-        bool SearchImgRec(int x, int y, List<ImgRec> LIR)
-        {
-            bool res = false;
-            for (int i = 0; i < LIR.Count; i++)
+            for (int j = 0; j < 2; j++)
             {
-                if (x >= LIR[i].x && x <= LIR[i].x + LIR[i].w && y >= LIR[i].y && y <= LIR[i].y + LIR[i].h)
+                LIR = SaveLoadImg.drawRect(j, loadingImg);
+                for (int i = 0; i < LIR.Count; i++)
                 {
-                    res = true;
+                    drawRectCanvas(LIR[i], j);
                 }
             }
-            return res;
-        }
 
-        bool SearchImgLine(int x, int y, List<ImgLine> LIL)
-        {
-            bool res = false;
-            for (int i = 0; i < LIL.Count; i++)
+            for (int j = 0; j < 2; j++)
             {
-                if (LIL[i].x1 != LIL[i].x2 && LIL[i].y1 != LIL[i].y2)
+                LIL = SaveLoadImg.drawLine(j, loadingImg);
+                for (int i = 0; i < LIL.Count; i++)
                 {
-                    if (x == ((LIL[i].x1 - LIL[i].x2) * (y - LIL[i].y1) / (LIL[i].y2 - LIL[i].y1)) + LIL[i].x2) 
-                    {
-                        res = true;
-                    }
+                    drawLineCanvas(LIL[i], j);
                 }
-                else
-                {
-                    if (LIL[i].x1 == LIL[i].x2)
-                    {
-                        if (x == LIL[i].x1 && ((y >= LIL[i].y1 && y <= LIL[i].y2) || (y <= LIL[i].y1 && y >= LIL[i].y2)))
-                        {
-                            res = true;
-                        }
-                    }
-                    else
-                    {
-                        if (y == LIL[i].y1 && ((x >= LIL[i].x1 && x <= LIL[i].x2) || (x <= LIL[i].x1 && x >= LIL[i].x2)))
-                        {
-                            res = true;
-                        }
-                    }
-                }
-
-
             }
-            return res;
+
         }
 
         Rectangle SearchRectBox(int x, int y, List<RectBox> LIR)
@@ -268,180 +217,52 @@ namespace XRect
             return res;
         }
 
-        bool serchP(int x, int y, List<Point> lp)
+        void drawRectCanvas(ImgRec ir, int type)
         {
-            bool b = true;
-
-            for (int i = 0; i < lp.Count; i++)
-            {
-                if (x == lp[i].X && y == lp[i].Y)
-                {
-                    b = false;
-                }
-            }
-
-            return b;
-        }
-
-        void drawRect(int type)
-        {
-            SolidColorBrush ColorBrush;
-
-            if(type == 0)
-            {
-                ColorBrush = Brushes.DarkRed;
-            }
-            else
-            {
-                ColorBrush = Brushes.Green;
-            }
-            
-
-            for (int i = 0; i < loadingImg.Height; i++)
-            {
-                for (int j = 0; j < loadingImg.Width; j++) 
-                {
-                    if (compareColor(loadingImg.GetPixel(j, i), ColorBrush) && !SearchImgRec(j, i, LIR)) 
-                    {
-                        int w = 0;
-                        int h = 0;
-                        while (compareColor(loadingImg.GetPixel(j, i + h), ColorBrush) || compareColor(loadingImg.GetPixel(j, i + h), Brushes.Black) || compareColor(loadingImg.GetPixel(j, i + h), Brushes.Blue) || compareColor(loadingImg.GetPixel(j, i + h), Brushes.Yellow))
-                        {
-                            h++;
-                        }
-
-                        while (compareColor(loadingImg.GetPixel(j + w, i), ColorBrush) || compareColor(loadingImg.GetPixel(j + w, i), Brushes.Black) || compareColor(loadingImg.GetPixel(j + w, i), Brushes.Blue) || compareColor(loadingImg.GetPixel(j + w, i), Brushes.Yellow))
-                        {
-                            w++;
-                        }
-
-                        if (w > 5 && h > 5)
-                        {
-                            ImgRec tIR = new ImgRec(j, i, h - 1, w - 1);
-                            LIR.Add(tIR);
-
-                            if (type == 0)
-                            {
-                                rect = new RectBoxRed();
-                            }
-                            else
-                            {
-                                rect = new RectBoxGreen();
-                            }
-
-                            LRB.Add(rect);
-
-                            this.cnvsMain.Children.Add(rect.rect);
-                            rect.rect.MouseDown += Rectangle_MouseDown_MainWindow;
-                            rect.rect.MouseUp += Rectangle_MouseUp_MainWindow;
-
-                            rect.rect.Width = w - 1 +9;
-                            rect.rect.Height = h - 1 +9;
-
-                            Canvas.SetLeft(rect.rect, j);
-                            Canvas.SetTop(rect.rect, i);
-                        }
-                    }
-                }
-
-            }
-        }
-
-        void drawLine(int type)
-        {
-            SolidColorBrush ColorBrush;
-
             if (type == 0)
             {
-                ColorBrush = Brushes.Blue;
+                rect = new RectBoxRed();
             }
             else
             {
-                ColorBrush = Brushes.Yellow;
+                rect = new RectBoxGreen();
             }
 
-            for (int i = 0; i < loadingImg.Height; i++)
-            {
-                for (int j = 0; j < loadingImg.Width; j++)
-                {
-                    if (compareColor(loadingImg.GetPixel(j, i), ColorBrush) && !SearchImgLine(j, i, LIL)) 
-                    {
-                        int w = 0; 
-                        int h = 0; 
+            LRB.Add(rect);
 
-                        List<Point> LP = new List<Point>();
+            this.cnvsMain.Children.Add(rect.rect);
+            rect.rect.MouseDown += Rectangle_MouseDown_MainWindow;
+            rect.rect.MouseUp += Rectangle_MouseUp_MainWindow;
 
-                        bool end = true;
+            rect.rect.Width = ir.w + 9;
+            rect.rect.Height = ir.h + 9;
 
-                        w = j; 
-                        h = i;
-                        LP.Add(new Point(w, h));
-
-                        while (end)
-                        {
-                            end = false;
-
-                            for (int i8 = -1; i8 < 2; i8++)
-                            {
-                                for (int j8 = -1; j8 < 2; j8++)
-                                {
-                                    if (compareColor(loadingImg.GetPixel(w + j8, h + i8), ColorBrush))
-                                    {
-                                        if (serchP(w + j8, h + i8, LP)) 
-                                        {
-                                            w = w + j8; 
-                                            h = h + i8;
-                                            LP.Add(new Point(w, h));
-                                            end = true;
-                                        }
-                                    }
-                                }
-                            }
-                        }
-
-                        ImgLine il = new ImgLine();
-                        il.x1 = j;
-                        il.y1 = i;
-
-                        il.x2 = w;
-                        il.y2 = h;
-
-                        LIL.Add(il);
-                        if (type == 0)
-                        {
-                            line = new LineBoxBlue();
-                        }
-                        else
-                        {
-                            line = new LineBoxYellow();
-                        }
-
-                        line.rect1 = SearchRectBox(LIL[LIL.Count - 1].x1, LIL[LIL.Count - 1].y1, LRB);
-                        line.rect2 = SearchRectBox(LIL[LIL.Count - 1].x2, LIL[LIL.Count - 1].y2, LRB); 
-                        if (line.rect1 != null && line.rect2 != null)
-                        {
-                            line.Rect1_Move();
-                            cnvsMain.MouseMove += line.Rect1_Move;
-
-                            line.Rect2_Move();
-                            cnvsMain.MouseMove += line.Rect2_Move;
-                            cnvsMain.Children.Add(line.lineMain);
-                        }
-                    }
-
-                }
-            }
+            Canvas.SetLeft(rect.rect, ir.x);
+            Canvas.SetTop(rect.rect, ir.y);
         }
 
-        bool compareColor(System.Drawing.Color c1, SolidColorBrush c2) 
+        void drawLineCanvas(ImgLine il, int type)
         {
-            bool Result = false;
-            if (c1.R == c2.Color.R && c1.B == c2.Color.B && c1.G == c2.Color.G)
+            if (type == 0)
             {
-                Result = true;
+                line = new LineBoxBlue();
             }
-            return Result;
-        }
+            else
+            {
+                line = new LineBoxYellow();
+            }
 
+            line.rect1 = SearchRectBox(il.x1, il.y1, LRB);
+            line.rect2 = SearchRectBox(il.x2, il.y2, LRB);
+            if (line.rect1 != null && line.rect2 != null)
+            {
+                line.Rect1_Move();
+                cnvsMain.MouseMove += line.Rect1_Move;
+
+                line.Rect2_Move();
+                cnvsMain.MouseMove += line.Rect2_Move;
+                cnvsMain.Children.Add(line.lineMain);
+            }
+        }
     }
 }
