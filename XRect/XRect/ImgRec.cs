3181ddf6433b0c1349cbb238d91e8733f9515c8a
mishgan95 <misha-varaksin10@mail.ru>
2016-02-06 00:16:03 +0700

    исправил

--- XRect/XRect/ImgRec.cs
+++ XRect/XRect/ImgRec.cs
@@ -8,7 +8,7 @@ namespace XRect
 {
     class ImgRec
     {
-        public int x { get; set; } 
+        public int x { get; set; }
         public int y { get; set; }
         public int h { get; set; }
         public int w { get; set; }
